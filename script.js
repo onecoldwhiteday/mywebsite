45a3424245adbcf52888b26053024b7056b7c88f
冷たい白い日 <coldwhiteday@localhost.localdomain>
2018-10-03 21:27:57 +0300

    fixed conditions in goNext and goprev functions and add key controls for slider

--- script.js
+++ script.js
@@ -5,38 +5,44 @@ let next = document.querySelector('.next');
 let prev = document.querySelector('.prev');
 
 prev.addEventListener('click', goPrev);
-
 next.addEventListener('click', goNext);
+
+window.addEventListener('keydown', function (event) {
+    if (event.keyCode == 39 && current != 4) {
+        goNext();
+    } else if (event.keyCode == 37 && current != 0) {
+        goPrev();
+    }
+})
 
 var current = 0;
 
 
 function renderSlides() {
     slides[0].classList.add('showMe');
+    prev.setAttribute('disabled', 'disabled');
 }
 
 renderSlides();
 
 function goNext() {
-    if (current == 4) {
-        next.setAttribute('disabled', 'disabled');
-    } else {
     prev.removeAttribute('disabled', 'disabled');
     hideCurrent();
     current++;
-    showAnother();
+    if (current == 4) {
+        next.setAttribute('disabled', 'disabled');
     }
+    showAnother();
 }
 
 function goPrev() {
-    if (current == 0) {
-        prev.setAttribute('disabled', 'disabled');
-    } else {
     next.removeAttribute('disabled', 'disabled');
     hideCurrent();
     current--;
+    if (current == 0) {
+        prev.setAttribute('disabled', 'disabled');
+    };
     showAnother();
-    }
 }
 
 function hideCurrent() {
