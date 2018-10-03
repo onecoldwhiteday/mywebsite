6a746a0fa24d73b73b3f860c5c5dd3c5aa2d23ef
Nik <zerganrus@gmail.com>
2018-10-03 23:19:24 +0300

    slide skills markup(fix it tomorrow), start to create slide contacts

--- script.js
+++ script.js
@@ -1,56 +1,62 @@
 let slides = document.querySelectorAll('.slide');
 
-
-let next = document.querySelector('.next');
-let prev = document.querySelector('.prev');
-
-prev.addEventListener('click', goPrev);
-next.addEventListener('click', goNext);
-
-window.addEventListener('keydown', function (event) {
-    if (event.keyCode == 39 && current != 4) {
-        goNext();
-    } else if (event.keyCode == 37 && current != 0) {
-        goPrev();
-    }
-})
-
 var current = 0;
 
 
-function renderSlides() {
+(function renderSlides() {
+
+    // CONTROLS BLOCK
+
+    let next = document.querySelector('.next');
+    let prev = document.querySelector('.prev');
+
+    prev.addEventListener('click', goPrev);
+    next.addEventListener('click', goNext);
+
+    window.addEventListener('keydown', function (event) {
+        if (event.keyCode == 39 && current != 4) {
+            goNext();
+        } else if (event.keyCode == 37 && current != 0) {
+            goPrev();
+        }
+    });
+
+
+    // ACTIONS BLOCK
+
     slides[0].classList.add('showMe');
     prev.setAttribute('disabled', 'disabled');
-}
 
-renderSlides();
-
-function goNext() {
-    prev.removeAttribute('disabled', 'disabled');
-    hideCurrent();
-    current++;
-    if (current == 4) {
-        next.setAttribute('disabled', 'disabled');
-    }
-    showAnother();
-}
-
-function goPrev() {
-    next.removeAttribute('disabled', 'disabled');
-    hideCurrent();
-    current--;
-    if (current == 0) {
-        prev.setAttribute('disabled', 'disabled');
+    function goNext() {
+        prev.removeAttribute('disabled', 'disabled');
+        hideCurrent();
+        current++;
+        if (current == 4) {
+            next.setAttribute('disabled', 'disabled');
+        }
+        showAnother();
     };
-    showAnother();
-}
+    
+    function goPrev() {
+        next.removeAttribute('disabled', 'disabled');
+        hideCurrent();
+        current--;
+        if (current == 0) {
+            prev.setAttribute('disabled', 'disabled');
+        };
+        showAnother();
+    };
+    
+    // INTERMEDIATE FUNCTIONS
 
-function hideCurrent() {
-    slides[current].classList.add('hideMe');
-    slides[current].classList.remove('showMe');
-}
+    function hideCurrent() {
+        slides[current].classList.add('hideMe');
+        slides[current].classList.remove('showMe');
+    };
+    
+    function showAnother() {
+        slides[current].classList.add('showMe');
+        slides[current].classList.remove('hideMe');
+    };
+}());
 
-function showAnother() {
-    slides[current].classList.add('showMe');
-    slides[current].classList.remove('hideMe');
-}
